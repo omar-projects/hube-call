9aa9ea87246f4ef8c3abb4516dcc226d7791f023
Kevin <balavoine.kevin@gmail.com>
2021-05-05 15:25:55 +0200

    TextBlob marche depuis Express JS

--- nodePythonApp/index.js
+++ nodePythonApp/index.js
@@ -1,29 +1,17 @@
 const express = require('express')
 const {spawn} = require('child_process');
-let {PythonShell} = require('python-shell');
 var package_name = 'textblob'
 var corpora = 'textblob.download_corpora';
 const app = express()
 const port = 3000
 
-let options = {
-    args : [package_name]
-}
-
-PythonShell.run('./install_package.py', 
-    function(err, results)
-    {
-        if (err) throw err;
-        else console.log(results);
-    });
-    
 app.get('/', (req, res) => {
 
  var dataToSend;
  // spawn new child process to call the python script
- const python = spawn('python', ['TextBlob/testBlob.py']);
+ const python = spawn('python3', ['TextBlob/testBlob.py']);
  // collect data from script
- python.stderr.on('data', function (data) {
+ python.stdout.on('data', function (data) {
   console.log('Pipe data from python script ...');
   dataToSend = data.toString();
  });
